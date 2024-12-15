7f76e4c2e4ec0704e065ec8f33bdc08d33a52ee3
yufutama <137602972+yufutama@users.noreply.github.com>
2024-12-15 20:58:38 +0900

    Add files via upload

---
+++ user/002/user-script.js
@@ -0,0 +1,12 @@
+function checkinfo() {
+    alert("このユーザーは運営から特別ユーザーとして指定されているため認証されています。")
+}
+function checkinfoy() {
+    alert("このユーザーは、戦闘ランキングの共同運営人であるため認証されています。")
+}
+function classinfo() {
+    alert("マスタースペシャルプラチナ階級は戦闘力が75億ポイントに達するとなる階級です。")
+}
+function classinfoy() {
+    alert("マスタースペシャルゴールドは、戦闘力が10億に達するとなる階級です。")
+}
